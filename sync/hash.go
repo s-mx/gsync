0ef0ff3f8425f12ad37132fafc64e1fd8b6acf63
Maxim Sabyanin <sabyanin.mx@gmail.com>
2021-02-12 23:06:49 +0300

    finish PolynomialHash implementation; add test for PolynomialHash

--- sync/hash.go
+++ sync/hash.go
@@ -20,12 +20,44 @@ func (h PolynomialHash) Append(ch int32, options PolynomialHashOptions) Polynomi
 }
 
 func (h PolynomialHash) PopLeft(ch int32, options PolynomialHashOptions) PolynomialHash {
-	return h
+	value := h.value
+	value -= (uint64(ch) * options.leftTerm) % options.modulo
+	if value >= options.modulo {
+		// value must be in the ring [0, modulo), so we return it in the ring in this branch.
+		value += options.modulo
+	}
+
+	return PolynomialHash{value}
 }
 
 type PolynomialHashOptions struct {
-	parameter	uint32
-	modulo		uint32
+	parameter	uint64
+	modulo		uint64
+
+	leftTerm		uint64
+}
+
+// binaryPow() function returns (a ^ power) % mod.
+// a, mod values should fit in int32.
+func binaryPow(a, power, mod int64) int64 {
+	if power == 0 {
+		return 1
+	} else if power % 2 == 0 {
+		tmp := binaryPow(a, power/2, mod)
+		return (tmp * tmp) % mod
+	} else {
+		return (binaryPow(a, power-1, mod) * a) % mod
+	}
+}
+
+func CreatePolynomialHashOptions(param, mod, windowLen int) PolynomialHashOptions {
+	leftTerm := binaryPow(int64(param), int64(windowLen-1), int64(mod))
+
+	return PolynomialHashOptions{
+		parameter: uint64(param),
+		modulo:    uint64(mod),
+		leftTerm:  uint64(leftTerm),
+	}
 }
 
 func CalculatePolynomialHashFromDataBlock(data string, options PolynomialHashOptions) PolynomialHash {
---
+++ sync/hash_test.go
@@ -0,0 +1,60 @@
+package sync
+
+import (
+	"math/rand"
+	"testing"
+)
+
+func generateData(size int) string {
+	var arr []byte = nil
+	for i := 0; i != size; i++ {
+		arr = append(arr, byte(rand.Int31n(127)))
+	}
+
+	return string(arr)
+}
+
+func calculatePolynomialHashByLoop(data string, start, end int, options PolynomialHashOptions) uint64 {
+	value := int64(0)
+	for start != end {
+		value = (value * int64(options.parameter)) % int64(options.modulo)
+		value += int64(data[start])
+		value %= int64(options.modulo)
+		start++
+	}
+
+	return uint64(value)
+}
+
+func TestPolynomialHash(t *testing.T) {
+	a := 123456789
+	mod := 1_000_000_000 + 7
+	windowLength := 10000
+	SIZE := 100000
+
+	options := CreatePolynomialHashOptions(a, mod, windowLength)
+	hash := PolynomialHash{value: 0}
+	data := generateData(SIZE)
+
+	windowStart := 0
+	i := 0
+	for ; i != windowLength; i++ {
+		hash = hash.Append(int32(data[i]), options)
+		correctValue := calculatePolynomialHashByLoop(data, windowStart, i+1, options)
+		if hash.value != correctValue {
+			t.Fatalf("Failed with indexes: [%d, %d), hash value: %d, correct hash value: %d",
+				windowStart, i+1, hash.value, correctValue)
+		}
+	}
+
+	for ; i != SIZE; i++ {
+		hash = hash.PopLeft(int32(data[windowStart]), options)
+		windowStart++
+		hash = hash.Append(int32(data[i]), options)
+		correctValue := calculatePolynomialHashByLoop(data, windowStart, i+1, options)
+		if hash.value != correctValue {
+			t.Fatalf("Failed with indexes: [%d, %d), hash value: %d, correct hash value: %d",
+				windowStart, i+1, hash.value, correctValue)
+		}
+	}
+}
